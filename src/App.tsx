4ce5bde7ab57e759a321a38fbf32a68d33ecf138
jessie-2023 <jessie.xie2023@gmail.com>
2025-03-28 10:36:06 +0000

    config to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.scss'
 import Home from './pages/Home'
 
